d8ec98d9a73c995f0b1fbbfc254b67ae0e5327b9
Cristian Dobre <cristian.dobre@hbfsrobotics.com>
2019-09-01 16:24:30 +0300

    Bugfix daemon thread

--- nodeconnector/nodeconnector.py
+++ nodeconnector/nodeconnector.py
@@ -4,69 +4,63 @@ Python connector module for Node.JS applications
 """
 __version__ = '0.9.0'
 
-
 import json
 import zmq
 import threading
 
-class Interface( threading.Thread ):
+
+class Interface(threading.Thread):
     # create
-    def __init__( self ):
+    def __init__(self):
         self.port = 24001
         self.socket_address = 'tcp://127.0.0.1:%d' % self.port
 
         # publisher socket
-        self.context = zmq.Context( )
-        self.socket = self.context.socket( zmq.REP )
-        
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+
         # routes
         self.router = {}
 
         # thread processing
-        threading.Thread.__init__ ( self )
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
 
     # task processing
-    def run( self ):
+    def run(self):
         # infinite loop
-        while( True ):
-            # stop thread
-            if( self.socket is None ):
-                break
-            
+        while(True):
             # query processing
-            if( self.socket.poll( 0 ) ):
-                query = json.loads( self.socket.recv() )
+            query = json.loads(self.socket.recv())
 
-                # return registered handlers
-                if( query[ '_p' ] == '__pyroutes' ):
-                    reply = [ k for k in self.router ]
-                    self.socket.send_json( dict( _p = query[ '_p' ], _id = query[ '_id' ], data = reply ) )
+            # return registered handlers
+            if(query['_p'] == '__pyroutes'):
+                reply = [k for k in self.router]
+                self.socket.send_json(
+                    dict(_p=query['_p'], _id=query['_id'], data=reply))
 
-                # check query path
-                elif( query[ '_p' ] in self.router ):
-                    fn, ctx = self.router[ query[ '_p' ] ] 
-                    args = {}
-                    if( 'args' in query ):
-                        args = query[ 'args' ]
+            # check query path
+            elif(query['_p'] in self.router):
+                fn, ctx = self.router[query['_p']]
+                args = {}
+                if('args' in query):
+                    args = query['args']
 
-                    # execute handler
-                    reply = fn( args, ctx )
-                        
-                    self.socket.send_json( dict( _p = query[ '_p' ], _id = query[ '_id' ], data = reply ) )
+                # execute handler
+                reply = fn(args, ctx)
+
+                self.socket.send_json(
+                    dict(_p=query['_p'], _id=query['_id'], data=reply))
 
     # routing queries
-    def handle( self, path, handle, context = {} ):
-        self.router[ path ] = ( handle, context )
- 
+
+    def handle(self, path, handle, context={}):
+        self.router[path] = (handle, context)
+
     # launch interface
-    def listen( self, port = 24001 ):
-        self.port = int( port )
+    def listen(self, port=24001):
+        self.port = int(port)
         self.socket_address = 'tcp://127.0.0.1:%d' % self.port
-        self.socket.bind( self.socket_address )
+        self.socket.bind(self.socket_address)
 
         self.start()
-
-    # stop listening on port
-    def end( self ):
-        self.socket.close()
-        self.socket = None
--- nodeconnector/tests/test_minimal.py
+++ nodeconnector/tests/test_minimal.py
@@ -2,19 +2,21 @@ import unittest
 
 import nodeconnector
 
-class TestMinimal( unittest.TestCase ):
+
+class TestMinimal(unittest.TestCase):
     """
     Arguments test
     """
-    def test_arguments( self ):
+
+    def test_arguments(self):
         api = nodeconnector.Interface()
         # default port
-        self.assertTrue( ( api.socket_address == 'tcp://127.0.0.1:24001' ) )
+        self.assertTrue((api.socket_address == 'tcp://127.0.0.1:24001'))
         # custom port
-        api.listen( 8080 )
-        self.assertTrue( ( api.socket_address == 'tcp://127.0.0.1:8080' ) )
+        api.listen(8080)
+        self.assertTrue((api.socket_address == 'tcp://127.0.0.1:8080'))
         # end socket
-        api.end()
+        # api.end()
 
 
 if __name__ == '__main__':
